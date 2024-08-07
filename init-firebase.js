1ba43d6df597344068145dae1949e8c3e04236fb
TowafH <towafh@nycstudents.net>
2024-08-07 11:59:50 -0400

    Add Firebase

--- init-firebase.js
+++ init-firebase.js
@@ -2,7 +2,15 @@
  * @TODO replace the firebaseConfig object below with the one
  * provided for you by firebase after creating your project!
  */
-const firebaseConfig = {};
+const firebaseConfig = {
+    apiKey: "AIzaSyBaQTqelsg2lX_AG_6zccshg2TyaDvZMUw",
+    authDomain: "fire-chat-44837.firebaseapp.com",
+    databaseURL: "https://fire-chat-44837-default-rtdb.firebaseio.com",
+    projectId: "fire-chat-44837",
+    storageBucket: "fire-chat-44837.appspot.com",
+    messagingSenderId: "310902923269",
+    appId: "1:310902923269:web:a489ed43ded0226e4bfc6c"
+};
 
 // initialize the firebase app with this built-in function
 firebase.initializeApp(firebaseConfig);
